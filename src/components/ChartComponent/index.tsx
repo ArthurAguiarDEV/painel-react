d43c6057ae11db4a6487062fee5e3a5d728fb159
Arthur Aguiar <arthuraguiardevbr@gmail.com>
2024-04-15 16:53:21 -0400

    terceiro commit

--- src/components/ChartComponent/index.tsx
+++ src/components/ChartComponent/index.tsx
@@ -1,37 +1,37 @@
-import Chart from "react-apexcharts"
+import Chart from "react-apexcharts";
 
 const ChartComponent = () => {
     const options = {
         chart: {
-            type: "area",
+            type: 'line', // Alterei 'area' para 'line' para corresponder ao tipo esperado
         },
         xaxis: {
             labels: {
-                formatter: function (value:any) {
+                formatter: function (value: any) {
                     const dates = [
-						"2024-01-01",
-						"2024-01-02",
-						"2024-01-03",
-						"2024-01-04",
-						"2024-01-05",
-						"2024-01-06",
-						"2024-01-07",
-						"2024-01-08",
-						"2024-01-09",
-					];
-					return dates[value];
+                        "2024-01-01",
+                        "2024-01-02",
+                        "2024-01-03",
+                        "2024-01-04",
+                        "2024-01-05",
+                        "2024-01-06",
+                        "2024-01-07",
+                        "2024-01-08",
+                        "2024-01-09",
+                    ];
+                    return dates[value];
                 }
             }
         }
     };
     const series = [
         {name:'sales', data:[30, 140, 85, 50, 90, 60, 70, 91, 125]}
-    ]
-  return (
-    <div className="w-full bg-white shadow-sm rounded-xl py-4 px-3">
-        <Chart options={options} series={series} type="area" height={350} />
-    </div>
-  )
-}
+    ];
+    return (
+        <div className="w-full bg-white shadow-sm rounded-xl py-4 px-3">
+            <Chart options={options} series={series} type="line" height={350} /> {/* Alterei 'area' para 'line' para corresponder ao tipo esperado */}
+        </div>
+    );
+};
 
-export default ChartComponent
+export default ChartComponent;
